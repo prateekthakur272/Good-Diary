2f894c9642304afc24d98d1b90bd32793c97a84a
Prateek Thakur <67188426+prateekthakur272@users.noreply.github.com>
2023-03-01 11:00:34 +0530

    sets up main activity with view model

--- app/src/main/java/com/prateekthakur272/gooddiary/MainActivity.kt
+++ app/src/main/java/com/prateekthakur272/gooddiary/MainActivity.kt
@@ -3,40 +3,57 @@ package com.prateekthakur272.gooddiary
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.view.GravityCompat
+import androidx.core.view.get
+import androidx.lifecycle.LifecycleOwner
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.navigation.NavigationView
 import com.prateekthakur272.gooddiary.adapter.PageAdapter
+import com.prateekthakur272.gooddiary.data.DataHelper
 import com.prateekthakur272.gooddiary.databinding.ActivityMainBinding
+import com.prateekthakur272.gooddiary.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel:MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
+        viewModel = MainViewModel(application)
+
         setContentView(binding.root)
 
-        binding.pageRecyclerView.adapter = PageAdapter(arrayListOf())
         binding.pageRecyclerView.layoutManager = LinearLayoutManager(this)
         binding.searchBar.buttonNavigationMenu.setOnClickListener {
             binding.navigationDrawer.openDrawer(binding.mainNavigationView)
         }
-
         binding.buttonAddPage.setOnClickListener {
             startActivity(Intent(this,PageActivity::class.java))
         }
         binding.mainNavigationView.setNavigationItemSelectedListener(this)
-        //binding.mainNavigationView.menu.getItem(0).subMenu!!.add("Personal diary").icon=AppCompatResources.getDrawable(this,R.drawable.ic_back)
 
+        viewModel.currentDiary.observe(this){
+            binding.pageRecyclerView.adapter = PageAdapter(viewModel.currentDiary.value!!)
+        }
+        viewModel.allDiaries.value?.forEachIndexed{ index,item ->
+            binding.mainNavigationView.menu[0].subMenu?.add(0,index,0,item)
+        }
     }
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
             R.id.create_new_diary-> {
-                Toast.makeText(this, "Create new diary",Toast.LENGTH_SHORT).show()
+                Toast.makeText(this,"Create",Toast.LENGTH_SHORT).show()
             }
-            else -> {}
+            else -> {
+                viewModel.loadDiary(viewModel.allDiaries.value?.get(item.itemId) ?: "")
+            }
         }
         binding.navigationDrawer.closeDrawer(GravityCompat.START)
         return true
--- app/src/main/java/com/prateekthakur272/gooddiary/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/prateekthakur272/gooddiary/viewmodel/MainViewModel.kt
@@ -1,5 +1,18 @@
 package com.prateekthakur272.gooddiary.viewmodel
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.prateekthakur272.gooddiary.data.DataHelper
 
-class MainViewModel:ViewModel()
+class MainViewModel(application: Application) :AndroidViewModel(application){
+    private var dataHelper: DataHelper = DataHelper(getApplication())
+    val allDiaries = MutableLiveData(dataHelper.getDiaries())
+    private var currentDiaryName = MutableLiveData(dataHelper.getDiaries()[0])
+    var currentDiary = MutableLiveData(dataHelper.getPages(currentDiaryName.value!!))
+
+    fun loadDiary(name:String){
+        currentDiaryName.value = name
+        currentDiary.value = dataHelper.getPages(name)
+    }
+}
